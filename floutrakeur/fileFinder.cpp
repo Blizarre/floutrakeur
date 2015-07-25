64973f1d4b37b19da50bf76c03ca48a90d99863b
Simon Marache-Francisco <git@marache.net>
2015-07-25 19:30:28 +0200

    still discovering boost: simplify code using native recursive call

--- floutrakeur/fileFinder.cpp
+++ floutrakeur/fileFinder.cpp
@@ -3,30 +3,18 @@
 using namespace boost::filesystem;
 using namespace std;
 
-void findRec(std::function<bool(string)> filter, vector<string>& matchingFiles, path& currentPath);
-
 
 FileFinder::FileFinder(string rootPath): m_rootPath(rootPath) { }
 
 std::vector<string> FileFinder::findAll(std::function<bool(string)> filter)
 {
 	std::vector<string> matchingFiles;
-	findRec(filter, matchingFiles, m_rootPath);
-	return matchingFiles;
-}
-
-void findRec(std::function<bool(string)> filter, vector<string>& matchingFiles, path& currentPath)
-{
-	for (path p : directory_iterator(currentPath))
+	for (path p : recursive_directory_iterator(m_rootPath))
 	{
-		if (is_directory(p))
-		{
-			findRec(filter, matchingFiles, p);
-		}
-		else
-			if (filter(p.filename().generic_string()))
-			{
-				matchingFiles.push_back( p.string() );
-			}
+        if (is_regular_file(p) && filter(p.filename().generic_string()))
+        {
+            matchingFiles.push_back( p.string() );
+        }
 	}
+	return matchingFiles;
 }
